de38a094c14a3ac22edd50060d3499632abb7411
immhj <109093282+immhj@users.noreply.github.com>
2022-10-21 20:26:42 +0800

    Add files via upload

---
+++ web_02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "time"
+
+type User_info struct {
+	id       string
+	hobby    string
+	name     string
+	password string
+	CreateAt time.Time
+	DeleteAt time.Time
+	UpdataAt time.Time
+	age      int
+	tel      int
+	email    string
+}
+
+func main() {
+
+}
